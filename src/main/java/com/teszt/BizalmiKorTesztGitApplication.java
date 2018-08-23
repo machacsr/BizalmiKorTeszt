37c97f9307fe82e1828993f592b052d498a873cf
machacsr <33865735+machacsr@users.noreply.github.com>
2018-08-23 14:18:18 +0200

    add RestController file

---
+++ src/main/java/com/teszt/BizalmiKorTesztGitApplication.java
@@ -0,0 +1,12 @@
+package com.teszt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizalmiKorTesztGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizalmiKorTesztGitApplication.class, args);
+	}
+}

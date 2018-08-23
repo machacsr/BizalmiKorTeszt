7fee6accf62dc0db2407cac2761d2c4f85a07a19
machacsr <33865735+machacsr@users.noreply.github.com>
2018-08-23 14:57:37 +0200

    update controller

--- src/main/java/com/teszt/ApiController.java
+++ src/main/java/com/teszt/ApiController.java
@@ -1,5 +1,17 @@
 package com.teszt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teszt.service.UserService;
+
+@RestController
 public class ApiController {
 
+	private UserService userService;
+	
+	@Autowired
+	public void setStoryService(UserService userService) {
+		this.userService = userService;
+	}
 }

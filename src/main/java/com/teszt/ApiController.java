66ed908d920d4cc8a52a24699c7c2855d1b85cf3
machacsr <33865735+machacsr@users.noreply.github.com>
2018-08-24 15:30:51 +0200

    Update api controller with rest api and userService dp injection

--- src/main/java/com/teszt/ApiController.java
+++ src/main/java/com/teszt/ApiController.java
@@ -1,8 +1,13 @@
 package com.teszt;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.teszt.entity.User;
 import com.teszt.service.UserService;
 
 @RestController
@@ -14,4 +19,22 @@ public class ApiController {
 	public void setStoryService(UserService userService) {
 		this.userService = userService;
 	}
+	
+	@RequestMapping("/addUser")
+    public boolean addUser(@RequestParam(value="UserName", required = true) String name, 
+    						@RequestParam(value="UserEmail", required = true) String email) {
+		return userService.saveUser(name, email);
+	}
+	
+	@RequestMapping("/deleteUser")
+    public boolean deleteUser(@RequestParam(value="UserId", required = true) Integer userId) {
+		return userService.deleteUser(userId);
+	}
+	
+	@RequestMapping("/getUsers")
+    public List<User> getUsers() {
+		return userService.getUsers();
+    }
+	
+	
 }

eb230ab9c545292274accf517ff6b4e5e873a669
machacsr <33865735+machacsr@users.noreply.github.com>
2018-08-24 15:32:44 +0200

    Add UserRepository to communicate database with jdbc under jpa

--- src/main/java/com/teszt/repository/UserRepository.java
+++ src/main/java/com/teszt/repository/UserRepository.java
@@ -6,6 +6,17 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.teszt.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Integer>{
+	
+//	static final String FIND_USERS_WITHOUT_PASSWORD = "Select user_id, user_name,  user_email from users";
+//	@Query(value = FIND_USERS_WITHOUT_PASSWORD, nativeQuery = true)
+//	public List<Object> findUsersWithoutPassword();
+	
+	void deleteByUserId(Integer userId); 
+	
+	boolean existsByUserEmail(String email);
+	
+	User findByUserEmail(String email);
+	
 	List<User> findAll();
 }

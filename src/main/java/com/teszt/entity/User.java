128b86c61bee040957a6d925e181d77c5db67879
machacsr <33865735+machacsr@users.noreply.github.com>
2018-08-24 15:31:21 +0200

    Add User pojo

--- src/main/java/com/teszt/entity/User.java
+++ src/main/java/com/teszt/entity/User.java
@@ -10,13 +10,64 @@ import javax.persistence.Table;
 @Table (name = "users")
 public class User {
 	
-	@Id @GeneratedValue
-	private Long id;
+	@Id @GeneratedValue @Column(unique = true)
+	private Integer userId;
 	
-	@Column(unique = true, nullable = false)
-	private String email;
+	@Column(unique = true, nullable = false, name = "userEmail")
+	private String userEmail;
 	
-	@Column(nullable = false)
-	private String password;
+	@Column(name = "userPassword")
+	private String userPassword;
+	
+	@Column(name = "userName")
+	private String userName;
+
+	public User() {
+	}
+	
+	public User(String userName, String userEmail) {
+		this.userName = userName;
+		this.userEmail = userEmail;
+	}
+	
+	public User(String userName, String userEmail, String userPassword) {
+		this.userName = userName;
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
 	
 }

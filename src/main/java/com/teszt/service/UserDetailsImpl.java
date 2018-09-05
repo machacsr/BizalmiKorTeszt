efa7f4df0aef9680bab224103a669c4493985c13
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-05 11:59:11 +0200

    update user details implementation for authenticaton

--- src/main/java/com/teszt/service/UserDetailsImpl.java
+++ src/main/java/com/teszt/service/UserDetailsImpl.java
@@ -1,10 +1,14 @@
 package com.teszt.service;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.teszt.entity.Role;
 import com.teszt.entity.User;
 
 @SuppressWarnings("serial")
@@ -18,7 +22,12 @@ public class UserDetailsImpl implements UserDetails {
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+		Set<Role> roles = user.getRoles();
+		for (Role role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role.getRole()));
+		}
+		return authorities;
 	}
 
 	@Override

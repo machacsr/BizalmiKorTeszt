7c0a32f2b71c44673c82709e655f40b987e5a33f
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-05 11:56:18 +0200

    update user entity to relationships

--- src/main/java/com/teszt/entity/User.java
+++ src/main/java/com/teszt/entity/User.java
@@ -1,26 +1,60 @@
 package com.teszt.entity;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table (name = "users")
 public class User {
 	
+	@JsonIgnore
 	@Id @GeneratedValue @Column(unique = true)
 	private Integer userId;
 	
+	@JsonIgnore
 	@Column(unique = true, nullable = false, name = "userEmail")
 	private String userEmail;
 	
+	@JsonIgnore
 	@Column(name = "userPassword")
 	private String userPassword;
 	
 	@Column(name = "userName")
 	private String userName;
+
+	@JsonBackReference
+	@ManyToMany( cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	@JoinTable( 
+		name = "users_roles", 
+		joinColumns = {@JoinColumn(name="user_id")}, 
+		inverseJoinColumns = {@JoinColumn(name="role_id")}  
+	)
+	private Set<Role> roles = new HashSet<Role>();
+	
+	@JsonBackReference
+	@OneToMany(mappedBy = "user")
+	private List<Story> stories;
+	
+	@JsonBackReference
+	@OneToMany(mappedBy = "user")
+	private List<Reservation> reservations;
+
 
 	public User() {
 	}
@@ -36,6 +70,7 @@ public class User {
 		this.userPassword = userPassword;
 	}
 
+	@JsonIgnore
 	public Integer getUserId() {
 		return userId;
 	}
@@ -44,6 +79,7 @@ public class User {
 		this.userId = userId;
 	}
 
+	@JsonIgnore
 	public String getUserEmail() {
 		return userEmail;
 	}
@@ -52,6 +88,7 @@ public class User {
 		this.userEmail = userEmail;
 	}
 
+	@JsonIgnore
 	public String getUserPassword() {
 		return userPassword;
 	}
@@ -67,7 +104,34 @@ public class User {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 	
+	public void addRoles(String roleName) {
+		if (this.roles == null || this.roles.isEmpty()) 
+			this.roles = new HashSet<>();
+		this.roles.add(new Role(roleName));
+	}
+
+	public List<Story> getStories() {
+		return stories;
+	}
+
+	public void setStories(List<Story> stories) {
+		this.stories = stories;
+	}
 	
-	
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
 }

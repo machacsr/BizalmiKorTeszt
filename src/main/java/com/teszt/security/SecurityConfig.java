4542833dc8c2773e3eb99e225dafb2c2a23ce513
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-05 11:57:54 +0200

    update security config

--- src/main/java/com/teszt/security/SecurityConfig.java
+++ src/main/java/com/teszt/security/SecurityConfig.java
@@ -3,24 +3,34 @@ package com.teszt.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.teszt.service.UserServiceImpl;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
-	private UserDetailsService userService;
+	private UserServiceImpl userService;
 	
 	@Autowired
 	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
-		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+		auth.authenticationProvider(authProvider());
+	}
+	 
+	@Bean
+	public DaoAuthenticationProvider authProvider() {
+	    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+	    authProvider.setUserDetailsService(userService);
+	    authProvider.setPasswordEncoder(passwordEncoder());
+	    return authProvider;
 	}
 	
 	@Bean
@@ -36,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/console/**").permitAll()
 			.antMatchers("/registration").permitAll()
 			.antMatchers("/reg").permitAll()
+			.antMatchers("/css/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 		.formLogin()

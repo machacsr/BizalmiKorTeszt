d163a96e32f06d555aaf65807e43953d311b3499
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-05 12:00:23 +0200

    update user service implementation

--- src/main/java/com/teszt/service/UserServiceImpl.java
+++ src/main/java/com/teszt/service/UserServiceImpl.java
@@ -11,7 +11,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.teszt.entity.Role;
 import com.teszt.entity.User;
+import com.teszt.repository.RoleRepository;
 import com.teszt.repository.UserRepository;
 
 @Service
@@ -19,22 +21,27 @@ import com.teszt.repository.UserRepository;
 public class UserServiceImpl implements UserService, UserDetailsService{
 
 	private UserRepository userRepository;
+	private RoleRepository roleRepository;
+	private PasswordEncoder passwordEncoder;
+	private final String USER_ROLE = "ROLE_USER";
+	private final String ADMIN_ROLE = "ROLE_ADMIN";
 	
 	@Autowired
-	public UserServiceImpl(UserRepository userRepository){
+	public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository){
 		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
 	}
 	
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-
+	public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = findByEmail(username);
 		if (user == null)
 			throw new UsernameNotFoundException(username);
-		user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
 		return new UserDetailsImpl(user);
 	}
 	
@@ -44,13 +51,37 @@ public class UserServiceImpl implements UserService, UserDetailsService{
 	}
 
 	@Override
-	public void registerUser(User user) {
-		userRepository.save(user);
+	public String registerUser(User userToRegister) {
+		User userCheck = userRepository.findByUserEmail(userToRegister.getUserEmail());
+
+		if (userCheck != null)
+			return "alreadyExists";
+
+		userToRegister.setUserPassword(passwordEncoder.encode(userToRegister.getUserPassword()));
+		
+		Role userRole = roleRepository.findByRole(USER_ROLE);
+		if (userRole != null) {
+			userToRegister.getRoles().add(userRole);
+		} else {
+			userToRegister.addRoles(USER_ROLE);
+		}
+		
+		Role adminRole = roleRepository.findByRole(ADMIN_ROLE);
+		if (adminRole != null) {
+			userToRegister.getRoles().add(adminRole);
+		} else {
+			userToRegister.addRoles(ADMIN_ROLE);
+		}
+		
+		userRepository.save(userToRegister);
+		
+		return "ok";
 	}
 
 	@Override
 	public List<User> getUsers() {
-		return userRepository.findAll();
+		List<User> users = userRepository.findAll();
+		return users;
 	}
 
 	@Override
@@ -71,6 +102,11 @@ public class UserServiceImpl implements UserService, UserDetailsService{
 		}else {
 			return true;
 		}
+	}
+
+	@Override
+	public User FindByUserId(Integer id) {
+		return userRepository.findByUserId(id);
 	}
 
 }

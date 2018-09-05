4d10af3d8b8a7537af90d08d06b12d6b3ef3b8df
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-05 11:52:31 +0200

    add controller to many things

--- src/main/java/com/teszt/HomeController.java
+++ src/main/java/com/teszt/HomeController.java
@@ -1,29 +1,46 @@
 package com.teszt;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.teszt.entity.User;
+import com.teszt.service.SailboatService;
 import com.teszt.service.UserService;
+
+import javassist.NotFoundException;
 
 @Controller
 public class HomeController {
-
-	private UserService userService;
 	
+	private UserService userService;
+	private SailboatService sailboatService;
+		
 	@Autowired
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
 	
+	@Autowired
+	public void setSailboatService(SailboatService sailboatService) {
+		this.sailboatService = sailboatService;
+	}
+
 	@RequestMapping("/registration")
 	public String registration(Model model){
 		model.addAttribute("user", new User());
 		return "registration";
+	}
+	
+	@RequestMapping("/")
+	public String stories() {
+		return "stories";
 	}
 	
 	@PostMapping("/reg") 
@@ -32,8 +49,22 @@ public class HomeController {
         return "auth/login";
     }
 	
-	@RequestMapping("/")
-	public String users() {;
-		return "main";
+	@RequestMapping("/sailboats")
+	public String sailboats(Model model) {
+		model.addAttribute("sailboats",sailboatService.getSailboats());
+		return "sailboats";
+	}
+	
+	@RequestMapping("/sailboats/reservation")
+	public String reservation(HttpServletRequest request,
+							  @RequestParam(value="SailboatName", required = true) String name,
+							   Model model) throws NotFoundException {
+		if (sailboatService.checkSailboatByName(name)){
+			model.addAttribute("sailboat", sailboatService.findSailboatByName(name));
+			return "reservation";
+		}else {
+			throw new NotFoundException("Nincs ilyen vitrolás az adatbázisunkban!");
+		}
+
 	}
 }

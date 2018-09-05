2ad55eac90cb2e65f7a2c9b25da3ea34f5eb9cad
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-05 11:58:15 +0200

    update webconfig

--- src/main/java/com/teszt/security/WebConfig.java
+++ src/main/java/com/teszt/security/WebConfig.java
@@ -2,10 +2,13 @@ package com.teszt.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
@@ -13,6 +16,21 @@ public class WebConfig implements WebMvcConfigurer {
 		WebMvcConfigurer.super.addViewControllers(registry);
 		registry.addViewController("/login").setViewName("auth/login");
 		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
 	}
 
 }

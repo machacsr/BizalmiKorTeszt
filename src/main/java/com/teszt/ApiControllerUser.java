79683827fcb2bbc567de3a4d5a4d10bdb928e481
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-13 22:20:17 +0200

    update api controller witch  has the user role

--- src/main/java/com/teszt/ApiControllerUser.java
+++ src/main/java/com/teszt/ApiControllerUser.java
@@ -1,13 +1,17 @@
 package com.teszt;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +24,6 @@ import com.teszt.service.ReservationService;
 import com.teszt.service.SailboatService;
 import com.teszt.service.StoryService;
 import com.teszt.service.UserService;
-import com.teszt.service.UserServiceImpl;
 
 @RestController
 public class ApiControllerUser {
@@ -60,20 +63,37 @@ public class ApiControllerUser {
 		return sailboatService.getSailboats();
 	}
 
-	@GetMapping(value = "/getReservations/{month}")
-	public List<Reservation> getReservationsByMonth(@PathVariable("month") byte month) {
-		return reservationService.getReservationsByMonth(month);
+	@GetMapping(value = "/getReservations/{month}/{sailboatName}")
+	public List<Reservation> getReservationsByMonth(@PathVariable("month") Integer month,
+			@PathVariable("sailboatName") String name) {
+		return reservationService.getReservationsByMonth(month,
+				sailboatService.findSailboatByName(name).getId().toString());
+	}
+
+	@GetMapping(value = "/getCurrentUserName")
+	public Map<String, String> getCurrentUserName(HttpServletRequest request) {
+		return Collections.singletonMap("user", userService.FindUserNameByEmail(request.getUserPrincipal().getName()));
 	}
 
 	@PostMapping("/addReservation")
-	public String addReservation(@RequestParam(value = "sailboatName", required = true) String sailboatName,
+	public Map<String, String> addReservation(
+			@RequestParam(value = "sailboatName", required = true) String sailboatName,
 			@RequestParam(value = "startTime", required = true) String startTime,
 			@RequestParam(value = "endTime", required = true) String endTime,
 			@RequestParam(value = "persons", required = true) String persons, HttpServletRequest request) {
-		reservationService.addReservation(sailboatService.findSailboatByName(sailboatName),
-				userService.findByEmail(request.getUserPrincipal().getName()),
-				startTime, endTime, persons);
-		return "ok";
+		return Collections.singletonMap("message",
+				reservationService.addReservation(sailboatService.findSailboatByName(sailboatName),
+						userService.findByEmail(request.getUserPrincipal().getName()), startTime, endTime, persons));
 	}
 
+	@GetMapping(value="/getReservations")
+	public List<Reservation> getReservations(HttpServletRequest request){
+		return reservationService.getReservationsByUserByYear(userService.findByEmail(request.getUserPrincipal().getName()));
+	}
+
+	@RequestMapping(value ="/deleteReservation/{reservationId}", method = RequestMethod.POST)
+	public boolean deleteReservation(@PathVariable Integer reservationId, HttpServletRequest request){
+		userService.findByEmail(request.getUserPrincipal().getName()).getReservations().forEach((res) -> res.toString());
+		return false;
+	}
 }

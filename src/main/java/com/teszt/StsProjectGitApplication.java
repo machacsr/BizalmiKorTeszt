af114f89e5e51a952ff8d879d3842d53f2486480
machacsr <33865735+machacsr@users.noreply.github.com>
2018-09-13 22:23:31 +0200

    rename main java file

---
+++ src/main/java/com/teszt/StsProjectGitApplication.java
@@ -0,0 +1,12 @@
+package com.teszt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsProjectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsProjectGitApplication.class, args);
+	}
+}
